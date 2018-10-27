2f397e7e0d4ba89920ecdcf417393a6304b18d44
qyhxxx <512202508@qq.com>
2018-10-27 17:55:15 +0800

    添加Cors中间件处理跨域

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Http;
 
+use App\Http\Middleware\Cors;
 use App\Http\Middleware\VerifyToken;
 use App\Http\Middleware\UseAdminGuard;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
@@ -21,6 +22,7 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         \App\Http\Middleware\TrustProxies::class,
+        Cors::class,
     ];
 
     /**
