6255415a9a9761a14748bc850154657de5fef8fb
qyhxxx <512202508@qq.com>
2019-04-02 14:39:42 +0800

    fix little bug

--- app/Http/Controllers/Admin/AuthController.php
+++ app/Http/Controllers/Admin/AuthController.php
@@ -10,7 +10,7 @@ class AuthController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('verifyToken', ['except' => ['register', 'login']]);
+        $this->middleware('verifyToken', ['except' => ['init', 'login']]);
     }
 
     public function init() {
