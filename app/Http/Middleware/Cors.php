6ed1dd78d66bba3dca93abd87bba2be758a935e3
qyhxxx <512202508@qq.com>
2019-04-06 00:05:49 +0800

    fix线上跨域

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -16,7 +16,8 @@ class Cors
     public function handle($request, Closure $next)
     {
         $response = $next($request);
-        $response->headers->set('Access-Control-Allow-Origin', 'http://localhost:8080');
+//        $response->headers->set('Access-Control-Allow-Origin', 'http://localhost:8080');
+        $response->headers->set('Access-Control-Allow-Origin', 'http://152.136.87.178:8080');
         $response->headers->set('Access-Control-Allow-Headers', 'Origin, Content-Type, Cookie, Accept, multipart/form-data, application/json, Authorization');
         $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, OPTIONS');
         $response->headers->set('Access-Control-Allow-Credentials', 'true');
